cae813c2a515bb3a35c8e0846cacaabc7f8d74d9
kanhasahu955 <ashoksahu8018183830@gmail.com>
2025-10-21 16:14:05 +0530

    reverse number

---
+++ Javascript/01.reverse_a_number.js
@@ -0,0 +1,11 @@
+function reverseNumber(num) {
+  let reversed = 0;
+  while (num > 0) {
+    let last_digit = num % 10
+    reversed = reversed * 10 + last_digit;
+    num = Math.floor(num / 10);
+  }
+  return reversed;
+}
+
+console.log(reverseNumber(12345))
